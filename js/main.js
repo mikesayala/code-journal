491f48143320000e3a4b1c07eefc5a67b4333650
Mike Ayala <ayala.s.mike@gmail.com>
2021-07-13 11:44:16 -0700

    Define a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML.

--- js/main.js
+++ js/main.js
@@ -24,4 +24,40 @@ function handleSubmitForm(event) {
   data.entries.unshift(formObject);
   $img.setAttribute('src', 'images/placeholder-image-square.jpg');
   $entryForm.reset();
+
+  objectDOMTree(formObject);
+}
+
+function objectDOMTree(formObject) {
+  var $ul = document.createElement('ul');
+  $ul.setAttribute('class', 'padding-left-0');
+
+  var $widthRow = document.createElement('div');
+  $widthRow.setAttribute('class', 'row width-row');
+
+  var $imageRow = document.createElement('div');
+  $imageRow.setAttribute('class', 'row image-row');
+
+  var $imgDOM = document.createElement('img');
+  $imgDOM.setAttribute('src', $entryForm.elements.url.value);
+  $imageRow.appendChild($imgDOM);
+  $widthRow.appendChild($imageRow);
+
+  var $columnHalf = document.createElement('div');
+  $columnHalf.setAttribute('class', 'column column-half');
+
+  var $h3 = document.createElement('h3');
+  $h3.setAttribute('class', 'label margin-left-20');
+  $h3.textContent = $entryForm.elements.title.value;
+  $columnHalf.appendChild($h3);
+
+  var $notesP = document.createElement('p');
+  $notesP.setAttribute('class', 'info photo-url padding-left-20');
+  $notesP.textContent = $entryForm.elements.notes.value;
+  $columnHalf.appendChild($notesP);
+
+  $widthRow.appendChild($columnHalf);
+  $ul.appendChild($imageRow);
+
+  return $ul;
 }
